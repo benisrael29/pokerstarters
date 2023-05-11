722952a991fc9569d2ffcf6f0ef2bfe082c8ba89
Ben Israel <israelben009@gmail.com>
2023-05-11 13:52:08 +1000

    Footer and other things

--- js/strategy_boxs.js
+++
@@ -1,9 +0,0 @@
-const toggleButtons = document.querySelectorAll('.toggle-button');
-
-toggleButtons.forEach(button => {
-  button.addEventListener('click', () => {
-    const toggleableContent = button.parentElement.parentElement.querySelector('.toggleable-content');
-    toggleableContent.classList.toggle('collapsed');
-    button.classList.toggle('collapsed');
-  });
-});
